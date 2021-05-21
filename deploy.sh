7f05de6ac0b44a20504c7b521e8619a410a04215
Adam Gardner <adam.gardner@magicmemories.com>
2021-05-20 14:46:36 -1000

    feature: Allow configuring --atomic, --wait, --debug, and more via input variables

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,38 @@ aws eks \
 
 # Helm Dependency Update
 # helm dependency update ${DEPLOY_CHART_PATH:-helm/}
-helm repo update
 helm repo add magic ${REPO} --username ${REPO_USERNAME} --password ${REPO_PASSWORD}
+helm repo update
 
-# Helm Deployment
-UPGRADE_COMMAND="helm upgrade --atomic --install --timeout ${TIMEOUT} --version ${VERSION}"
+UPGRADE_COMMAND="helm upgrade --install"
+
+if [[ "$ATOMIC" = "true" ]]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --atomic"
+elif [[ "$ATOMIC" != "false" ]]; then
+    echo "ERROR: 'atomic' input value should be either 'true' or 'false', got ${ATOMIC}"
+    exit 1
+fi
+
+if [[ "$WAIT" = "true" ]]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --wait"
+elif [[ "$WAIT" != "false" ]]; then
+    echo "ERROR: 'wait' input value should be either 'true' or 'false', got ${WAIT}"
+    exit 1
+fi
+
+if [[ "$DEBUG" = "true" ]]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --debug"
+elif [[ "$DEBUG" != "false" ]]; then
+    echo "ERROR: 'debug' input value should be either 'true' or 'false', got ${DEBUG}"
+    exit 1
+fi
+
+UPGRADE_COMMAND="${UPGRADE_COMMAND} --timeout ${TIMEOUT} --version ${VERSION}"
+
+if [ -n "$EXTRA_ARGS" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} ${EXTRA_ARGS}"
+fi
+
 for config_file in ${DEPLOY_CONFIG_FILES//,/ }
 do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
